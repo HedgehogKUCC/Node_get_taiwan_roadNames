c154423b3ce95c0d912251075630c5f74744a882
HedgehogKU <ccc908925@gmail.com>
2021-05-09 11:34:25 +0800

    get data result records & add fileName .json

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const https = require('https');
 const exportFromJSON = require('export-from-json');
 
 const API_URL = 'https://od.moi.gov.tw/api/v1/rest/datastore/301000000A-000917-031?limit=50000';
-let data = '';
-const FILE_NAME = 'download';
-const EXPORT_TYPE = 'txt';
+let jsonData = '';
+let objData = {};
+const FILE_NAME = 'download.json';
+const EXPORT_TYPE = 'json';
 
 http.createServer(function (request, response) {
     https.get(API_URL, (res) => {
@@ -14,21 +15,22 @@ http.createServer(function (request, response) {
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
             try {
-                data = rawData;
+                objData = JSON.parse(rawData);
+                jsonData = JSON.stringify(objData.result.records);
             } catch (e) {
                 console.error(e.message);
             } finally {
                 let result = exportFromJSON({
-                    data,
-                    FILE_NAME,
-                    EXPORT_TYPE,
+                    data: jsonData,
+                    fileName: FILE_NAME,
+                    exportType: EXPORT_TYPE,
                     processor(content, type, fileName) {
                         switch(type) {
                             case 'txt':
                                 response.setHeader('Content-Type', 'text/plain');
                                 break;
                             case 'json':
-                                response.setHeader('Content-Type', 'text/plain');
+                                response.setHeader('Content-Type', 'application/json');
                                 break;
                             case 'csv':
                                 response.setHeader('Content-Type', 'text/csv');
